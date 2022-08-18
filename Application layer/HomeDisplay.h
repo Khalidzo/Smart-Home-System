7f7950aad65f9ad6bcfae95380a71974866d95e2
Khaled Abdelsalam <kh.salama2001@gmail.com>
2022-08-18 11:02:29 +0200

    Added function prototypes

--- Application layer/HomeDisplay.h
+++ Application layer/HomeDisplay.h
@@ -3,9 +3,8 @@
 #ifndef HOMEDISPLAY_H_
 #define HOMEDISPLAY_H_
 
-/*******************************************_INCLUDES_***************************************/
-
-
-
+/*******************************************_PROTOTYPES_***************************************/
+void sendWelcomeMessage(void);
+void displayTemperature(void);
 
 #endif /* HOMEDISPLAY_H_ */
