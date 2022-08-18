0e56b62b6d9032b09545970ae8b3b4fd976e23b5
Khaled Abdelsalam <kh.salama2001@gmail.com>
2022-08-18 10:44:46 +0200

    Added Temperature check in Tempsensor.c

--- HAL/TempSensor/TempSensor.c
+++ HAL/TempSensor/TempSensor.c
@@ -8,5 +8,12 @@ void TempSensor_Init(void){
 u16  TempSensor_Read(void){
 	u16 ADCValue  = ADC_Read();
 	u16 Temperature = ((u32)ADCValue * 500) / 1023;
+	if(Temperature > 35){
+		DCMotor_Start();
+		Buzzer_Beep();
+	}
+	else {
+		DCMotor_Stop();
+	}
 	return Temperature;
 }
--- HAL/TempSensor/TempSensor.h
+++ HAL/TempSensor/TempSensor.h
@@ -5,6 +5,8 @@
 
 /******************************************_INCLUDES_***************************************/
 #include "ADC.h"
+#include "Buzzer.h"
+#include "DCMotor.h"
 /*****************************************_PROTOTYPES_***************************************/
 void TempSensor_Init(void);
 u16  TempSensor_Read(void);
