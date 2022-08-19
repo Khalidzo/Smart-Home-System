616f12bb8cf581d75cd9864b37c8861273331782
Khaled Salama <78038233+Khalidzo@users.noreply.github.com>
2022-08-19 13:43:04 +0200

    Delete Debug directory

--- Debug/HAL/EEPROM/EEPROM.d
+++
@@ -1,9 +0,0 @@
-HAL/EEPROM/EEPROM.d HAL/EEPROM/EEPROM.o: ../HAL/EEPROM/EEPROM.c \
- ../HAL/EEPROM/EEPROM.h ../Application\ layer/StdTypes.h \
- ../Application\ layer/BitMath.h
-
-../HAL/EEPROM/EEPROM.h:
-
-../Application\ layer/StdTypes.h:
-
-../Application\ layer/BitMath.h:
